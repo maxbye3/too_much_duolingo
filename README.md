2243c68dfa0d7581a20f0cceac56373387fdc999
Max Bye <maxbye@gmail.com>
2023-01-13 18:43:16 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# chekhov_hwacha
+A music video for some music I made! Includes lots of trains and maps.
+
+## How is it done?
+Simple two videos with a click that makes one go to opacity 0. 
+Few JS commands to pause, play and sync the videos.
+
+### Would have loved
+To have open maps do all the maps bits but the renders take too long.
---
+++ js/index.js
@@ -0,0 +1,45 @@
+const mainVideo = document.getElementById('mainVideo')
+const mapVideo = document.getElementById('mapVideo')
+let initialClick = true
+
+setInterval(() => {
+  mapVideo.currentTime = mainVideo.currentTime
+}, 100)
+
+// on mobile
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  // some code..
+  document.getElementById('interactionType').remove()
+}
+
+const touchdown = () => {
+  mainVideo.play()
+  mapVideo.play()
+  if (initialClick) {
+    document.getElementById('disclaimer').style.display = 'none'
+    initialClick = false
+    return
+  }
+  mainVideo.style.opacity = 0
+  // console.log('touch down')
+}
+
+const removeDisclaimer = () => {
+  document.getElementById('mapMode').remove()
+}
+
+const touchup = () => {
+  mainVideo.style.opacity = 100
+  // console.log('touch up')
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  window.addEventListener('mousedown', touchdown, false)
+  window.addEventListener('mouseup', touchup, false)
+  window.addEventListener('touchstart', touchdown, false)
+  window.addEventListener('touchend', touchup, false)
+})
