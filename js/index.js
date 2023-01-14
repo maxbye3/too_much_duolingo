0381fae1f65ebee69ee663fe48513fd6b3646d3a
maxbye3 <maxbye@gmail.com>
2023-01-14 09:40:40 +0200

    video changes

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
 const mainVideo = document.getElementById('mainVideo')
-const mapVideo = document.getElementById('mapVideo')
+// const mapVideo = document.getElementById('mapVideo')
 let initialClick = true
 
-setInterval(() => {
-  mapVideo.currentTime = mainVideo.currentTime
-}, 100)
+// setInterval(() => {
+//   mapVideo.currentTime = mainVideo.currentTime
+// }, 100)
 
-// on mobile
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  // some code..
-  document.getElementById('interactionType').remove()
-}
+// // on mobile
+// if (
+//   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+//     navigator.userAgent
+//   )
+// ) {
+//   // some code..
+//   document.getElementById('interactionType').remove()
+// }
 
 const touchdown = () => {
   mainVideo.play()
-  mapVideo.play()
+  // mapVideo.play()
   if (initialClick) {
     document.getElementById('disclaimer').style.display = 'none'
     initialClick = false
     return
   }
-  mainVideo.style.opacity = 0
+  // mainVideo.style.opacity = 0
   // console.log('touch down')
 }
 
 const removeDisclaimer = () => {
-  document.getElementById('mapMode').remove()
+  document.getElementById('downloadMusicDisclaimer').remove()
 }
 
-const touchup = () => {
-  mainVideo.style.opacity = 100
-  // console.log('touch up')
-}
+// const touchup = () => {
+//   mainVideo.style.opacity = 100
+//   // console.log('touch up')
+// }
 
 document.addEventListener('DOMContentLoaded', function (event) {
   window.addEventListener('mousedown', touchdown, false)
